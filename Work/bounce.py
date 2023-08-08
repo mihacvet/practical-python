43a563ae58708719df394630f7ba14dd8f909789
Mihailo Cvetic <mihailo.cvetic@gmail.com>
2023-08-08 21:50:37 +0200

    Modify bounce.py - Complete excersise 1.5; Add sears.py - Practice example problem;

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+bounce_nr = 1
+regression = 0.6
+
+while bounce_nr <= 10:
+    height *= regression
+    print("Bounce:", bounce_nr, "current height: ", round(height, 4))
+    bounce_nr += 1
